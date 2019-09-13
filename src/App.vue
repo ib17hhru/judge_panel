75f8dafe8356a4b9e775c8ea7e55fbf91fa3a861
zlumer <zlumer@gmail.com>
2019-09-14 00:48:46 +0300

    load data before app load

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,43 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+	<div id="app">
+		<div id="nav">
+			<router-link to="/">Home</router-link> |
+			<router-link to="/about">About</router-link>
+		</div>
+		<router-view/>
+	</div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+	data: function()
+	{
+		return { }
+	},
+	mounted()
+	{
+		this.$store.dispatch('LOAD_DATA')
+	}
+})
+</script>
 
 <style lang="less">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
 }
 #nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+	padding: 30px;
+	a {
+		font-weight: bold;
+		color: #2c3e50;
+		&.router-link-exact-active {
+			color: #42b983;
+		}
+	}
 }
 </style>
