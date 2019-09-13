7985f9b8b428456d68fe2c00d5a93acea62dc550
zlumer <zlumer@gmail.com>
2019-09-14 00:46:38 +0300

    store data loading

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,29 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
+import { ITrackRecord } from "./interfaces"
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  }
+	state: {
+		tracks: [] as ITrackRecord[]
+	},
+	mutations: {
+		UPDATE_DATA(state, payload: { tracks: ITrackRecord[] })
+		{
+			state.tracks = payload.tracks
+			console.log(payload.tracks.slice(0, 10))
+		}
+	},
+	actions: {
+		async LOAD_DATA()
+		{
+			let res = await axios('/all.json')
+			let data = res.data as ITrackRecord[]
+			data.sort((a,b) => a.id - b.id)
+			data.forEach(x => x.path = `https://ib17.hip-hop.ru${x.path}`)
+			this.commit('UPDATE_DATA', { tracks: data })
+		}
+	}
 })
