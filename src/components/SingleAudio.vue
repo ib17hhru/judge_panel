2fe4e388b78d4571047d2522117e5e36c265fa44
zlumer <zlumer@gmail.com>
2019-09-14 00:48:32 +0300

    auto-stopping audio component

---
+++ src/components/SingleAudio.vue
@@ -0,0 +1,27 @@
+<template>
+	<audio
+		controls
+		:src="src"
+		@play.capture="onAudioPlay"
+	>
+		Your browser does not support the
+		<code>audio</code> element.
+	</audio>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { stopAllAudioExcept } from "../utils"
+
+export default Vue.extend({
+	props: {
+		src: String
+	},
+	methods: {
+		onAudioPlay(e: Event)
+		{
+			stopAllAudioExcept(e.target as HTMLAudioElement)
+		}
+	}
+})
+</script>
