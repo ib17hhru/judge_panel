f1593fe5d7cddc69e3cfb0ee32385c2fdec1e0fd
zlumer <zlumer@gmail.com>
2019-09-15 16:53:27 +0300

    judging vuex store

--- src/store.ts
+++ src/store.ts
@@ -5,18 +5,94 @@ import { ITrackRecord } from "./interfaces"
 
 Vue.use(Vuex)
 
+export interface ITrackRating
+{
+	id: number
+
+	score: number
+	comment: string
+
+	score552?: string
+}
+
 export default new Vuex.Store({
 	state: {
-		tracks: [] as ITrackRecord[]
+		inited: false,
+		tracks: [] as ITrackRecord[],
+		trackObj: {} as { [key: number]: ITrackRecord },
+		judging: {
+			currentTrackId: NaN,
+			ratings: [] as ITrackRating[],
+			notRatedIds: [] as number[],
+		}
 	},
 	mutations: {
+		SET_INITED(state, { inited }: { inited: boolean })
+		{
+			state.inited = inited
+		},
 		UPDATE_DATA(state, payload: { tracks: ITrackRecord[] })
 		{
 			state.tracks = payload.tracks
+			state.trackObj = payload.tracks.reduce((acc, cur) => ({ ...acc, [cur.id]: cur }), { } as typeof state.trackObj)
 			console.log(payload.tracks.slice(0, 10))
-		}
+		},
+		JUDGE_SET_CURRENT_TRACK(state, { id }: { id: number })
+		{
+			state.judging.currentTrackId = id
+		},
+		JUDGE_SET_RATINGS(state, { ratings }: { ratings: ITrackRating[] })
+		{
+			state.judging.ratings = ratings
+		},
+		JUDGE_ADD_RATING(state, { rating }: { rating: ITrackRating })
+		{
+			state.judging.ratings.push(rating)
+		},
+		JUDGE_SELECT_NEW_TRACK({ judging }, { idx }: { idx: number })
+		{
+			let oldTrackId = judging.currentTrackId
+			let [newTrackId] = judging.notRatedIds.splice(idx, 1)
+			if (!isNaN(oldTrackId))
+				judging.notRatedIds.push(oldTrackId)
+			
+			judging.currentTrackId = newTrackId
+		},
+		JUDGE_POPULATE_NOT_RATED(state)
+		{
+			let knownTrackIds = [
+				...state.judging.ratings.map(x => x.id),
+				...(isNaN(state.judging.currentTrackId) ? [] : [state.judging.currentTrackId]),
+				...state.judging.notRatedIds
+			]
+			let newTracks = state.tracks.filter(x => !knownTrackIds.includes(x.id))
+			state.judging.notRatedIds.push(...newTracks.map(x => x.id))
+		},
 	},
 	actions: {
+		STORAGE_LOAD_JUDGE()
+		{
+			const J_SAVE = { cur: null as null | number, ratings: [] as ITrackRating[] }
+			let ju = JSON.parse(localStorage.getItem('judge') || JSON.stringify(J_SAVE)) as typeof J_SAVE
+			let cur = (typeof ju.cur === "number") ? ju.cur : NaN
+			this.commit('JUDGE_SET_CURRENT_TRACK', { id: cur })
+			this.commit('JUDGE_SET_RATINGS', { ratings: ju.ratings })
+		},
+		STORAGE_SAVE_JUDGE()
+		{
+			let data = {
+				cur: this.state.judging.currentTrackId,
+				ratings: this.state.judging.ratings,
+			}
+			localStorage.setItem('judge', JSON.stringify(data))
+		},
+		async INIT()
+		{
+			await this.dispatch('LOAD_DATA')
+			await this.dispatch('STORAGE_LOAD_JUDGE')
+			this.commit('JUDGE_POPULATE_NOT_RATED')
+			this.commit('SET_INITED', { inited: true })
+		},
 		async LOAD_DATA()
 		{
 			let res = await axios('/all.json')
@@ -24,6 +100,20 @@ export default new Vuex.Store({
 			data.sort((a,b) => a.id - b.id)
 			data.forEach(x => x.path = `https://ib17.hip-hop.ru${x.path}`)
 			this.commit('UPDATE_DATA', { tracks: data })
+		},
+		async JUDGE_SELECT_NEW_TRACK()
+		{
+			if (!this.state.judging.notRatedIds.length)
+				return
+			
+			let idx = Math.floor(Math.random() * this.state.judging.notRatedIds.length)
+			this.commit('JUDGE_SELECT_NEW_TRACK', { idx })
+			await this.dispatch('STORAGE_SAVE_JUDGE')
+		},
+		async JUDGE_INIT()
+		{
+			if (isNaN(this.state.judging.currentTrackId))
+				await this.dispatch('JUDGE_SELECT_NEW_TRACK')
 		}
 	}
 })
